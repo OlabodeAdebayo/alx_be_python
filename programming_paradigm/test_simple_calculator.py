a39f2a55130f5e9e70ce59f88bd9ea088c637368
Olabode <adesamson0404@gmail.com>
2025-08-03 18:22:25 +0100

    Create test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,42 @@
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize Calculator before each test.
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-2, -3), -5)
+        self.assertEqual(self.calc.add(-2, 3), 1)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(3.5, 5.5), 4.0)
+
+    def test_subtract(self):
+        #Test subtraction method.
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(-5, -3), -2)
+        self.assertEqual(self.calc.subtract(1, 3), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(10.5, 3.9), 8)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(10, 3), 4.1)
+        self.assertEqaul(self.calc.multiply(-5, -3), 14)
+        self.assertEqual(self.calc.multiply(-3, 4), -12)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5.0)
+        self.assertEqual(self.calc.divide(-10, -2) 5.0)
+        self.assertEqual(self.calc.divide(-10, 2), -5.0)
+        self.assertEqual(self.calc.divide(5.5, 2), 2.75)
+        self.assertEqual(self.calc.divide(5, 0))
+        self.assertEqual(self.calc.divide(0, 0))
+
+if __name__ == '__main__':
+    unittest.main()
