2ecd2580b77ee91fa6cf09cbefbcba37b791ca99
Olabode <adesamson0404@gmail.com>
2025-08-10 19:36:24 +0100

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -12,7 +12,7 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        print("Deleting (title of the book)")
+            print(f"Deleting {self.title}")
 
 if __name__ == "__main__":
     main()
